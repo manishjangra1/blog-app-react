4658e6a48a59dd8d0d80b56886d49fd6f57dc571
manishjangra1 <dev.jangramanish@gmail.com>
2025-01-18 18:36:00 +0530

    nextjs migration done

--- src/app/layout.js
+++ src/app/layout.js
@@ -73,3 +73,5 @@ export default function RootLayout({ children }) {
 // minor tweak
 
 // minor tweak
+
+// minor tweak
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -52,3 +52,5 @@ export default PostCard
 // minor tweak
 
 // minor tweak
+
+// minor tweak
