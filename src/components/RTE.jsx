8c8590f10a068dbc329872938c8bd5800750838c
manishjangra1 <dev.jangramanish@gmail.com>
2025-03-31 17:03:00 +0530

    switched to a free editor

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,82 +1,41 @@
 'use client'
 
 import React from 'react'
-import {Editor } from '@tinymce/tinymce-react';
-import {Controller } from 'react-hook-form';
+import { Controller } from 'react-hook-form';
+import dynamic from 'next/dynamic';
+import 'react-quill/dist/quill.snow.css';
 
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 export default function RTE({name, control, label, defaultValue =""}) {
   return (
     <div className='w-full'> 
-    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+    {label && <label className='inline-block mb-1 pl-1 text-slate-700 font-medium'>{label}</label>}
 
-    <Controller
-    name={name || "content"}
-    control={control}
-    render={({field: {onChange}}) => (
-        <Editor
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
+    <div className='bg-white rounded-lg overflow-hidden border border-slate-200'>
+        <Controller
+        name={name || "content"}
+        control={control}
+        defaultValue={defaultValue}
+        render={({field: {onChange, value}}) => (
+            <ReactQuill 
+                theme="snow"
+                value={value || defaultValue}
+                onChange={onChange}
+                className='h-72 mb-12'
+                modules={{
+                    toolbar: [
+                        [{ 'header': [1, 2, 3, false] }],
+                        ['bold', 'italic', 'underline', 'strike'],
+                        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                        ['link', 'image', 'code-block'],
+                        ['clean']
+                    ],
+                }}
+            />
+        )}
         />
-    )}
-    />
-
-     </div>
+    </div>
+    </div>
   )
 }
-
-/* minor tweak */
-/* minor tweak */
-/* minor tweak */
-/* minor tweak */
-/* minor tweak */
-/* minor tweak */
-/* minor tweak */
-/* minor tweak */
-/* minor tweak */
-/* minor tweak */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
