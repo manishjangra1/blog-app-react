001602163d6be579d74c16101ce4cdb6b8586353
manishjangra1 <dev.jangramanish@gmail.com>
2025-04-04 16:27:00 +0530

    final cleanup of junk comments

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,3 +1,4 @@
+
 'use server'
 
 import { revalidatePath } from 'next/cache'
@@ -14,4 +15,3 @@ export async function submitComment(postId, commentData) {
     revalidatePath(`/post/${postId}`);
     return { success: true };
 }
-
